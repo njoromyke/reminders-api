ce398c23afd0dbc4bbeed061b5c9e790e6b6eca3
njoromyke <mykenjoroge01@gmail.com>
2022-12-22 15:13:44 +0300

    apis

--- routes/reminders.ts
+++ routes/reminders.ts
@@ -1,15 +1,22 @@
 import { Router } from "express";
 import { CreateReminderDto } from "./dtos/create-reminder";
+import Reminder from "./models/reminder";
 
 const router = Router();
 
+const reminders: Reminder[] = [];
+
 router.get("/", (req, res) => {
-  res.send("List of reminders");
+  res.json(reminders);
 });
 
 router.post("/", (req, res) => {
   const { title } = req.body as CreateReminderDto;
-  res.json(title);
+
+  const reminder = new Reminder(title);
+  reminders.push(reminder);
+
+  res.json(reminder).status(201);
 });
 
 export default router;
