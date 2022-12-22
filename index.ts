31c7767d07db3d3f55c9f2cd692a0371792f4fdc
njoromyke <mykenjoroge01@gmail.com>
2022-12-22 14:59:17 +0300

    router

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import remindersRouters from "./routes/reminders";
 
 const app = express();
+
+app.use("/reminders", remindersRouters);
 
 app.get("/", (req, res) => {
   res.send("Hello world");
